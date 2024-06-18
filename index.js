bb0219d8995886b68264aa081c4aeb0af42921b9
mehakrastogi <149245124+Mehak-rastogi@users.noreply.github.com>
2024-06-18 14:46:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var typed = new Typed(".text", {
-//     strings: ["Frontend Developer", "Web Developer"],
-//     typeSpeed: 200,
-//     backSpeed: 200,
-//     backDelay: 1000,
-//     loop: true,
-//   });
-// });
 document.addEventListener("DOMContentLoaded", function () {
   const toggleSidebar = document.getElementById("toggleSidebar");
   const sidebar = document.querySelector(".sidebar");
 
   toggleSidebar.addEventListener("click", function () {
-    sidebar.classList.remove("sm:invisible");
-    sidebar.classList.toggle("sm:hidden");
-    sidebar.classList.toggle("sm:right-0.5");
+    sidebar.classList.remove("invisible");
+    sidebar.classList.toggle("hidden");
+    sidebar.classList.toggle("right-0.5");
     const list = document.querySelectorAll("li");
     list.forEach((li) => {
       li.addEventListener("click", function () {
-        sidebar.classList.add("sm:invisible");
+        sidebar.classList.add("invisible");
       });
     });
   });
